40ee712c1bcafac78f790d915e75c20e90edd4ab
syq0806 <61413903+syq0806@users.noreply.github.com>
2022-08-26 23:14:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,3 +28,7 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Ma
 sed -i 's/1.2022.37/1.2022.37.1/g' feeds/packages/net/smartdns/Makefile
 sed -i 's/5a2559f0648198c290bb8839b9f6a0adab8ebcdc/96d3deb595eae4f849cb2a51c69a9be89947fa0d/g' feeds/packages/net/smartdns/Makefile
 sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
+
+########### 安装smartdns（必选）###########
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+#git clone https://github.com/pymumu/smartdns.git package/smartdns
