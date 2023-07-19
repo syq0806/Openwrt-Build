611aed426e9517896c050146c5ee947537d8925f
syq0806 <61413903+syq0806@users.noreply.github.com>
2023-07-19 14:56:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,4 +37,5 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-a
 # git clone https://github.com/pymumu/smartdns.git package/smartdns
 ########### 安装adguardhome（必选）###########
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-
+########### 安装ddns-go（必选）###########
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
