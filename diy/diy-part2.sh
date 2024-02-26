a45191167f2c7a3f608a9f08771e5b6318c5daca
syq0806 <61413903+syq0806@users.noreply.github.com>
2024-02-26 11:31:58 +0800

    Update diy-part2.sh

--- diy/diy-part2.sh
+++ diy/diy-part2.sh
@@ -11,12 +11,12 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_generate
 ########### 设置密码为空（可选） ###########
 # sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
-# Update Golang
-git clone -b master --single-branch https://github.com/immortalwrt/packages.git packages_master
-rm -rf ./feeds/packages/lang/golang
-mv ./packages_master/lang/golang ./feeds/packages/lang
+##取消bootstrap为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
 
