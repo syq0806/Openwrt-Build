38c41f6ce2d8ad799ab7c3ff8a856f342d1dc46c
syq0806 <61413903+syq0806@users.noreply.github.com>
2024-03-01 21:28:40 +0800

    Update diy-part1.sh

--- diy/mt7986/diy-part1.sh
+++ diy/mt7986/diy-part1.sh
@@ -9,10 +9,7 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-####### 更换packages源 #######
-sed -i 's/^src-git-full packages https:\/\/github.com\/immortalwrt\/packages.git;openwrt-21.02/#&/' feeds.conf.default
-sed -i 's/^src-git-full luci https:\/\/github.com\/immortalwrt\/luci.git;openwrt-21.02/#&/' feeds.conf.default
-echo 'src-git packages https://github.com/kenzok8/small-package.git;main' >>feeds.conf.default
+
 ####### 科学上网插件 #######
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
 # echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-dev' >>feeds.conf.default
