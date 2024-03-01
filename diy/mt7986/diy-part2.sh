49a1c32fb03d0e2d84979d2bd327f87e585a0176
syq0806 <61413903+syq0806@users.noreply.github.com>
2024-03-01 21:39:03 +0800

    Update diy-part2.sh

--- diy/mt7986/diy-part2.sh
+++ diy/mt7986/diy-part2.sh
@@ -22,7 +22,8 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-n
 curl -sL https://raw.githubusercontent.com/pymumu/smartdns/master/package/openwrt/Makefile > feeds/packages/net/smartdns/Makefile
 
 ########### 更新内置的passwall版本 ###########
-# rsync -a feeds/passwall_luci/luci-app-passwall/  feeds/luci/applications/luci-app-passwall/
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone -b luci-smartdns-dev https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 # pushd feeds/packages/lang
 # rm -rf golang && svn co https://github.com/sbwml/packages_lang_golang/tree/22.x/golang
 # popd
