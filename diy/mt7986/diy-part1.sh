4a80307d0b826125662d87a85cba904bf14708bd
syq0806 <61413903+syq0806@users.noreply.github.com>
2024-03-01 08:54:04 +0800

    Update diy-part1.sh

--- diy/mt7986/diy-part1.sh
+++ diy/mt7986/diy-part1.sh
@@ -12,6 +12,6 @@
 
 
 ####### 科学上网插件 #######
-# echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
-# echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-dev' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-dev' >>feeds.conf.default
 # echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
