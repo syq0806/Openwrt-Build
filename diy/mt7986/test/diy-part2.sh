0374363f9920159d1ae15ca6d786f2cff068cf16
syq0806 <61413903+syq0806@users.noreply.github.com>
2024-03-13 11:36:13 +0800

    Update diy-part2.sh

--- diy/mt7986/test/diy-part2.sh
+++ diy/mt7986/test/diy-part2.sh
@@ -35,8 +35,6 @@ do
  echo "Deleted $cmd"
 done
 
-./scripts/feeds update -a && ./scripts/feeds install -a
-
 ##取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
