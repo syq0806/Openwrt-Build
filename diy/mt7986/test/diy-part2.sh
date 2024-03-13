bee2e391bc4f7b2c5d9554ff8e5e1002648614f9
syq0806 <61413903+syq0806@users.noreply.github.com>
2024-03-13 11:34:10 +0800

    Update diy-part2.sh

--- diy/mt7986/test/diy-part2.sh
+++ diy/mt7986/test/diy-part2.sh
@@ -35,6 +35,8 @@ do
  echo "Deleted $cmd"
 done
 
+./scripts/feeds update -a && ./scripts/feeds install -a
+
 ##取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
@@ -43,6 +45,3 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-n
 ########### 更新内置的golang版本 ###########
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-
-##复制passwall  ###########
-cp feeds/passwall_luci/luci-app-passwall feeds/luci/applications -r
