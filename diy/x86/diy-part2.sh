2bb23ea69864813a2b33ec5eedf666b5febc4c56
syq0806 <61413903+syq0806@users.noreply.github.com>
2024-03-20 16:47:40 +0800

    Update diy-part2.sh

--- diy/x86/diy-part2.sh
+++ diy/x86/diy-part2.sh
@@ -52,8 +52,8 @@ done
 
 
 ##取消bootstrap为默认主题
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
 
 ########### 更新内置的golang版本 ###########
