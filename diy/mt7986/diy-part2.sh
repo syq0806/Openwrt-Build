72b506d3ad053b8c3515cd8829ca5641f0586fed
syq0806 <61413903+syq0806@users.noreply.github.com>
2024-08-24 19:32:29 +0800

    Update diy-part2.sh

--- diy/mt7986/diy-part2.sh
+++ diy/mt7986/diy-part2.sh
@@ -37,6 +37,8 @@ do
  echo "Deleted $cmd"
 done
 
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_generate
 
 ##取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
