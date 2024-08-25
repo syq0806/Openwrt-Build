722fff9968a3a92b1568b0885c9e74b8c9c94705
syq0806 <61413903+syq0806@users.noreply.github.com>
2024-08-25 10:56:26 +0800

    Update diy-part1.sh

--- diy/mt7986/test/diy-part1.sh
+++ diy/mt7986/test/diy-part1.sh
@@ -10,10 +10,11 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-####### 科学上网插件 #######
+# Add a feed source
+sed -i '$a src-git openwrt_kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+
+####### passwall smartnds分支 #######
 git clone https://github.com/xiaorouji/openwrt-passwall -b luci-smartdns-dev package/luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/luci-app-passwall-packages
 # curl -s https://raw.githubusercontent.com/sbwml/openwrt_helloworld/v5/shadowsocks-rust/Makefile | sed 's/^/ /' > package/luci-app-passwall-packages/shadowsocks-rust/Makefile
-####### Smartdns插件 #######
-git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
-git clone https://github.com/pymumu/luci-app-smartdns -b master package/luci-app-smartdns
+
